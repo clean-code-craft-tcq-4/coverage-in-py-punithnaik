4a4dd9c8df1d97b716a907c27280df660e182538
Naik Punith <pnu5kor@bosch.com>
2022-07-15 15:49:08 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -8,7 +8,10 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.infer_breach(101, 50, 100) == 'TOO_HIGH')
     self.assertTrue(typewise_alert.infer_breach(51, 50, 100) == 'NORMAL')
 
-
+  def test_classify_temperature_breach(self):
+    self.assertTrue(typewise_alert.classify_temperature_breach('PASSIVE_COOLING', -1) == 'TOO_LOW')
+    self.assertTrue(typewise_alert.classify_temperature_breach('HI_ACTIVE_COOLING', 46) == 'TOO_HIGH')
+    self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', 1) == 'NORMAL')
 
 if __name__ == '__main__':
   unittest.main()
