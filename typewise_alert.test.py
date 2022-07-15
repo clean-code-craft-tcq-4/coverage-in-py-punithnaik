1df4d0878a0aae61963c504226c52105a4d3f417
Naik Punith <pnu5kor@bosch.com>
2022-07-15 17:53:46 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -14,15 +14,27 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.classify_temperature_breach('HI_ACTIVE_COOLING', 46) == 'TOO_HIGH')
     self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', 1) == 'NORMAL')
 
+  #Test with valid values of alertTarget
   #Mocking the functions send_to_controller() and send_to_email() for unit testing purpose
   @patch('typewise_alert.send_to_controller')
   @patch('typewise_alert.send_to_email')
-  def test_check_and_alert(self,mock_send_to_email,mock_send_to_controller):
+  def test_check_and_alert_with_valid_value(self,mock_send_to_email,mock_send_to_controller):
     batteryChar = {'coolingType':''}
     typewise_alert.check_and_alert('TO_CONTROLLER', batteryChar,0)
     mock_send_to_controller.assert_called_once()
     typewise_alert.check_and_alert('TO_EMAIL', batteryChar,0)
     mock_send_to_email.assert_called_once()
+
+  #Test with invalid value for alertTarget
+  #Mocking the functions send_to_controller() and send_to_email() for unit testing purpose
+  @patch('typewise_alert.send_to_controller')
+  @patch('typewise_alert.send_to_email')
+  def test_check_and_alert_with_invalid_value(self,mock_send_to_email,mock_send_to_controller): 
+    batteryChar = {'coolingType':''}
+    typewise_alert.check_and_alert('', batteryChar,0) #For else coverage
+    mock_send_to_controller.assert_not_called()
+    typewise_alert.check_and_alert('', batteryChar,0) #For else coverage
+    mock_send_to_email.assert_not_called()
 
   #Mocking print function
   @patch('builtins.print')
@@ -30,13 +42,21 @@ class TypewiseTest(unittest.TestCase):
     typewise_alert.send_to_controller('TOO_LOW')
     mock_print.assert_called_with('65261, TOO_LOW')
 
+  #Test with valid values of breachType
   #Mocking print function
   @patch('builtins.print')
-  def test_send_to_email(self,mock_print):
+  def test_send_to_email_with_valid_value(self,mock_print):
     typewise_alert.send_to_email('TOO_LOW')
     mock_print.assert_called_with('Hi, the temperature is too low')
     typewise_alert.send_to_email('TOO_HIGH')
     mock_print.assert_called_with('Hi, the temperature is too high')
+
+  #Test with invalid value for breachType
+  #Mocking print function
+  @patch('builtins.print')
+  def test_send_to_email_with_invalid_value(self,mock_print):
+    typewise_alert.send_to_email('') #For else coverage
+    mock_print.assert_not_called()
     
 if __name__ == '__main__':
   unittest.main()
