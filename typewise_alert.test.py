6785f011ad54816b397133ecbfa87a9cda74fcf6
Naik Punith <pnu5kor@bosch.com>
2022-07-15 17:02:38 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -1,5 +1,6 @@
 import unittest
 import typewise_alert
+from unittest.mock import patch 
 
 
 class TypewiseTest(unittest.TestCase):
@@ -13,5 +14,15 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.classify_temperature_breach('HI_ACTIVE_COOLING', 46) == 'TOO_HIGH')
     self.assertTrue(typewise_alert.classify_temperature_breach('MED_ACTIVE_COOLING', 1) == 'NORMAL')
 
+  #Mocking the functions send_to_controller() and send_to_email() for unit testing purpose
+  @patch('typewise_alert.send_to_controller')
+  @patch('typewise_alert.send_to_email')
+  def test_check_and_alert(self,mock_send_to_email,mock_send_to_controller):
+    batteryChar = {'coolingType':''}
+    typewise_alert.check_and_alert('TO_CONTROLLER', batteryChar,0)
+    mock_send_to_controller.assert_called_once()
+    typewise_alert.check_and_alert('TO_EMAIL', batteryChar,0)
+    mock_send_to_email.assert_called_once()
+    
 if __name__ == '__main__':
   unittest.main()
